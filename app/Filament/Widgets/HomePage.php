9f1c82cf32913eac75610d5a880bae38ba4533d6
iamnoor <noor.itmajlis@gmail.com>
2023-03-26 03:26:22 +0530

    add widget in admin panel dashboard for goto home page

---
+++ app/Filament/Widgets/HomePage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class HomePage extends Widget
+{
+    protected static string $view = 'filament.widgets.home-page';
+}
---
+++ resources/views/filament/widgets/home-page.blade.php
@@ -0,0 +1,5 @@
+<x-filament::widget>
+    <x-filament::card>
+        <a href="{{ env('APP_URL') }}" target="_blank" style="text-decoration: underline;font-size:2em;">&#8680; Goto Home Page</a>
+    </x-filament::card>
+</x-filament::widget>
